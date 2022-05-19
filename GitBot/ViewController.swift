22a6d8b46e243bf2ed0d1f6cfee88761731ec46a
Ahmad Yasser <42544598+ahmdyasser@users.noreply.github.com>
2022-05-19 05:11:37 +0200

    Initial Commit

---
+++ GitBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitBot
+//
+//  Created by Ahmad Yasser on 19/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
